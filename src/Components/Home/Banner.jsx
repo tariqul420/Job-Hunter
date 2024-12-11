4def8bf73e5a7de7dc7b5232e5eea0b0bca640cf
tariqul420 <tariqul.islam.programmer@gmail.com>
2024-12-11 19:09:39 +0600

    added motion img on banner

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -1,6 +1,8 @@
 // react icons
-import {CiSearch} from "react-icons/ci";
-import {FaCircleCheck} from "react-icons/fa6";
+import { CiSearch } from "react-icons/ci";
+import { motion } from "motion/react";
+import group1 from "../../assets/Motion/group1.jpg"
+import group2 from "../../assets/Motion/group2.jpg"
 
 const Banner = () => {
 
@@ -9,87 +11,25 @@ const Banner = () => {
 
             {/* header */}
             <header className="flex lg:flex-row flex-col gap-[50px] lg:gap-10 items-center p-8">
-                <div className="w-full lg:w-[55%]">
-                    <h1 className="text-[40px] sm:text-[60px] font-[600] leading-[45px] sm:leading-[70px]">Let
-                        your <span className="text-[#F38160]">groceries</span> come to you</h1>
-                    <p className="text-[18px] text-gray-400 mt-2">Get fresh groceries online without stepping out to
-                        make delicious food with the freshest ingredients</p>
+                <div className="w-full lg:w-[40%]">
+                    <h1 className="text-[40px] sm:text-[60px] font-[600] leading-[45px] sm:leading-[70px]">The <span className="text-color-primary">Easiest Way</span> to Get Your New Job</h1>
+                    <p className="text-[18px] text-gray-400 mt-2">Each month, more than 3 million job seekers turn to website in their search for work, making over 140,000 applications every single day</p>
                     <div className="relative my-5">
                         <input placeholder="Search here"
-                               className="py-3 px-4 w-full outline-none rounded-l-md bg-gray-100"/>
+                            className="py-3 px-4 w-full outline-none rounded-l-md bg-gray-100" />
                         <button
                             className="h-full absolute top-0 right-0 bg-[#F38160] px-3 text-white text-[1.3rem] rounded-r-md">
-                            <CiSearch/></button>
-                    </div>
-
-                    <div className="grid grid-cols-1 400px:grid-cols-2 gap-[15px] w-full sm:w-[80%]">
-                        <p className="flex items-center gap-[5px] text-gray-400 text-[1rem]">
-                            <FaCircleCheck className="text-[#F0B70D] text-[1.2rem]"/>
-                            Fresh Vegetables
-                        </p>
-                        <p className="flex items-center gap-[5px] text-gray-400 text-[1rem]">
-                            <FaCircleCheck className="text-[#F0B70D] text-[1.2rem]"/>
-                            100% Guarantee
-                        </p>
-                        <p className="flex items-center gap-[5px] text-gray-400 text-[1rem]">
-                            <FaCircleCheck className="text-[#F0B70D] text-[1.2rem]"/>
-                            Cash on Delivery
-                        </p>
-                        <p className="flex items-center gap-[5px] text-gray-400 text-[1rem]">
-                            <FaCircleCheck className="text-[#F0B70D] text-[1.2rem]"/>
-                            Fast Delivery
-                        </p>
+                            <CiSearch /></button>
                     </div>
                 </div>
 
-                <div className="w-full sm:w-[40%]">
-                    <img src="https://i.ibb.co/61R7zbv/delivery-concept-handsome-african-american-deliver-AZUZTZ3-1.png"
-                         alt="image" className="w-full h-full"/>
+                <div className="w-full sm:w-[55%]">
+                    <motion.img animate={{ y: [100, 150, 100] }} transition={{ duration: 10, repeat: Infinity }} src={group1} alt="image" className="rounded-t-[40px] rounded-br-[40px] border-l-8 border-b-8 border-color-primary w-[400px]" />
+
+                    <motion.img animate={{ x: [100, 150, 100] }} transition={{ duration: 10, repeat: Infinity, delay: 5 }} src={group2} alt="image" className="rounded-t-[40px] w-[400px] rounded-br-[40px] border-l-8 border-b-8 border-color-primary" />
+
                 </div>
             </header>
-
-            <section className="p-8 mt-16 grid sm:grid-cols-2 grid-cols-1 lg:grid-cols-4 gap-[25px] flex-wrap">
-                <div>
-                    <img src="https://i.ibb.co/XCM2bhM/Baby-food.png" alt="icon"
-                         className="w-[60px]"/>
-                    <h4 className="text-[18px] font-[500]">Honey</h4>
-                    <p className="text-[14px] leading-[18px] text-gray-400 font-[300]">Lorem ipsum
-                        dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-
-                <div>
-                    <img src="https://i.ibb.co/J5Yd3cZ/Condiments.png" alt="icon"
-                         className="w-[60px]"/>
-                    <h4 className="text-[18px] font-[500]">Spices & Seasonings</h4>
-                    <p className="text-[14px] leading-[18px] text-gray-400 font-[300]">Lorem ipsum
-                        dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-
-                <div>
-                    <img src="https://i.ibb.co/h2R9kny/Dairy.png" alt="icon"
-                         className="w-[60px]"/>
-                    <h4 className="text-[18px] font-[500]">Dairy Products</h4>
-                    <p className="text-[14px] leading-[18px] text-gray-400 font-[300]">Lorem ipsum
-                        dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-
-                <div>
-                    <img src="https://i.ibb.co/HYHZfHQ/Grain-and-pasta.png" alt="icon"
-                         className="w-[60px]"/>
-                    <h4 className="text-[18px] font-[500]">Flour</h4>
-                    <p className="text-[14px] leading-[18px] text-gray-400 font-[300]">Lorem ipsum
-                        dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-
-                <div>
-                    <img src="https://i.ibb.co/y5ZTLHv/Fruits-and-vegetables.png" alt="icon"
-                         className="w-[60px]"/>
-                    <h4 className="text-[18px] font-[500]">Vegetables & Fruits</h4>
-                    <p className="text-[14px] leading-[18px] text-gray-400 font-[300]">Lorem ipsum
-                        dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-
-            </section>
         </div>
     );
 };
